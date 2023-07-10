d5039d92da6c6a16c1d8a461ef63fbbe0d75b27a
Kobiman <kobiman91@gmail.com>
2023-07-10 06:43:15 +0000

    Agent Report

--- AMS/Shared/Dto/SalesDetails.cs
+++ AMS/Shared/Dto/SalesDetails.cs
@@ -6,5 +6,5 @@ using System.Threading.Tasks;
 
 namespace AMS.Shared.Dto
 {
-    public record SalesDetails(string? AccountId, string? AgentId, string? Name, decimal DailySales, string? Description, DateTime? EntryDate, decimal WinAmount, string? ReceiptNumber);
+    public record SalesDetails(string? AccountId, string? AgentId, string? Name, decimal DailySales, string? Description, DateTime? EntryDate, decimal WinAmount, string? ReceiptNumber, decimal OpeningBalance, decimal EndBalance);
 }
