bb2f65482830320b1a6773d1062fed5835fa34da
Kobiman <kobiman91@gmail.com>
2023-10-30 19:12:58 +0000

    SendBulkSMS

--- AMS/Server/Services/NotificationService.cs
+++ AMS/Server/Services/NotificationService.cs
@@ -16,9 +16,15 @@ namespace AMS.Server.Services
             var response = await client.PostAsync(endPoint, data).ConfigureAwait(false);
         }
 
-        public Task SendBulkSMS(string message, string[] contacts)
+        public async Task SendBulkSMS(string message, string[] contacts)
         {
-            throw new NotImplementedException();
+            var req = new { sender = "AMS", message, recipients = contacts };
+            var json = JsonConvert.SerializeObject(req);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var endPoint = $"https://sms.arkesel.com/api/v2/sms/send";
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Add("api-key", "OjZqeWo4QlFOSEc5V2xwb2g");
+            var response = await client.PostAsync(endPoint, data).ConfigureAwait(false);
         }
     }
 }
