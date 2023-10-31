3bd36a52a46244e01bc00b10db60576b6c53376d
rehotechgh <rehotechgh@gmail.com>
2023-10-31 12:29:27 +0000

    SMS KEY
    KEY

--- AMS/Server/Services/NotificationService.cs
+++ AMS/Server/Services/NotificationService.cs
@@ -7,23 +7,23 @@ namespace AMS.Server.Services
     {
         public async Task SendSMS(string message, string contact)
         {
-            var req = new { sender = "AMS", message, recipients = new string[] { contact } };
+            var req = new { sender = "VAG-OBIRI", message, recipients = new string[] { contact } };
             var json = JsonConvert.SerializeObject(req);
             var data = new StringContent(json, Encoding.UTF8, "application/json");
-            var endPoint = $"https://sms.arkesel.com/api/v2/sms/send";
+            var endPoint = $"https://smsc.hubtel.com/v1/messages/send";
             using HttpClient client = new();
-            client.DefaultRequestHeaders.Add("api-key", "OjZqeWo4QlFOSEc5V2xwb2g");
+            client.DefaultRequestHeaders.Add("api-key", "vngouhbn&clientid=qxbqqmkn");
             var response = await client.PostAsync(endPoint, data).ConfigureAwait(false);
         }
 
         public async Task SendBulkSMS(string message, string[] contacts)
         {
-            var req = new { sender = "AMS", message, recipients = contacts };
+            var req = new { sender = "VAG-OBIRI", message, recipients = contacts };
             var json = JsonConvert.SerializeObject(req);
             var data = new StringContent(json, Encoding.UTF8, "application/json");
-            var endPoint = $"https://sms.arkesel.com/api/v2/sms/send";
+            var endPoint = $"https://smsc.hubtel.com/v1/messages/send";
             using HttpClient client = new();
-            client.DefaultRequestHeaders.Add("api-key", "OjZqeWo4QlFOSEc5V2xwb2g");
+            client.DefaultRequestHeaders.Add("api-key", "vngouhbn&clientid=qxbqqmkn");
             var response = await client.PostAsync(endPoint, data).ConfigureAwait(false);
         }
     }
