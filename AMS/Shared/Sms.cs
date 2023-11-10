0201efd18781b41b216f452526d0908ee8ed9b73
jewel <tracejewel01@gmail.com>
2023-11-10 13:23:11 +0000

    SmsUpdate

---
+++ AMS/Shared/Sms.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMS.Shared.Dto
+{
+    public class Sms
+    {
+        [Required]
+        public string msg { get; set; } = "";
+
+        [Required]
+        public string Phone { get; set; }
+
+    }
+}
