db8d7982ec80a4be084f6dd7b682d3a91ea1e166
Kobiman <kobiman91@gmail.com>
2024-03-08 10:29:53 +0000

    ..

--- AMS/Shared/Dto/SalesDto2.cs
+++ AMS/Shared/Dto/SalesDto2.cs
@@ -7,4 +7,5 @@ using System.Threading.Tasks;
 namespace AMS.Shared.Dto
 {
    public record SalesDto2(string AccountId, string AgentId, decimal DailySales, string Description, DateTime? EntryDate, DateTime? DrawDate, decimal WinAmount, string GameId, string ReceiptNumber,string Id);
+   public record WinsDto(decimal WinAmount,string SalesId);
 }
