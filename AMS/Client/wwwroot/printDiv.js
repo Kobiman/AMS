c6c309483191b643848d442944319e1aa9710de9
jewel <tracejewel01@gmail.com>
2024-03-09 15:40:43 +0000

    PrintPayout

---
+++ AMS/Client/wwwroot/printDiv.js
@@ -0,0 +1,10 @@
+﻿function printDiv() {
+    var divContents = document.getElementById("PrintDiv").innerHTML;
+    var a = window.open('', '', 'height=500, width=250');
+    a.document.write('<html>');
+    a.document.write('<body > <br>');
+    a.document.write(divContents);
+    a.document.write('</body></html>');
+    a.document.close();
+    a.print();
+}
