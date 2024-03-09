73a70521ce7a3446cca3eecbb11eb76d1583eed4
Kobiman <kobiman91@gmail.com>
2024-03-09 14:07:08 +0000

    ..

--- AMS/Server/Services/SalesService.cs
+++ AMS/Server/Services/SalesService.cs
@@ -122,7 +122,7 @@ namespace AMS.Server.Services
                                 from gme in _gme.DefaultIfEmpty()
                                 where
                                 t.EntryDate >= startDate.Date && t.EntryDate <= endDate.Date
-                                && t.Description.Equals("balance b/f",StringComparison.OrdinalIgnoreCase)
+                                && t.Description == "balance b/f"
 
                                 select new SalesDto
                                 {
