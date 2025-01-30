a34005ff09908ff47296a57653cbf9cbc05af34a
jewel <tracejewel01@gmail.com>
2025-01-30 13:59:02 +0000

    WinsLocation

---
+++ AMS/Server/Migrations/20250130131233_WinsLocaion.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AMS.Server.Migrations
+{
+    public partial class WinsLocaion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LocationId",
+                table: "Wins",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "Wins");
+        }
+    }
+}
